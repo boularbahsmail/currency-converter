e36726c2ac35b33312b7e120ec137eb2501e95a1
ismail boularbah <qiranorane55@gmail.com>
2020-05-16 06:43:39 +0000

    first commit

---
+++ Scripts/config.js
@@ -0,0 +1,8 @@
+﻿var require = {
+	baseUrl: "",
+	paths: {
+		'jquery': './Scripts/jquery', // JQuery
+		'MindFusion.Common': './Scripts/MindFusion.Common',
+		'MindFusion.Gauges': './Scripts/MindFusion.Gauges',
+	},
+};
